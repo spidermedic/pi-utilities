9f834c55ab5cbfa3f0a797adcc742165f0c96f35
Doug <dougmartinrn@gmail.com>
2019-12-26 02:32:26 -0500

    Added timout variable and comments

--- pir.py
+++ pir.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 
-# Imported Modules
+# Import Modules
 from time import sleep
 import subprocess
 import RPi.GPIO as GPIO
 
 
-# initialize GPIO
+# Initialize Variables
+PIR = 22          # Data pin that the PIR sensor is connected to
+timeout = 30      # How long to wait after no movement is detected to turn off the screen
+display_on = True # Defaults the screen to "on"
+
+# Initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
-GPIO.setup(22, GPIO.IN)
+GPIO.setup(PIR, GPIO.IN)
 
-PIR = 22
-display_on = True
-
+# Main Function
 while True:
 
     if display_on and not GPIO.input(PIR):
@@ -24,6 +27,6 @@ while True:
     if not display_on and GPIO.input(PIR):
         subprocess.run("vcgencmd display_power 1 > /dev/null 2>&1", shell=True)
         display_on = True
-        sleep(30)
+        sleep(timeout)
 
     sleep(0.5)
