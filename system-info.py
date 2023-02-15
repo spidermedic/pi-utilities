fa1745b5e34161d5510064e0b8a8182dffec73fe
Doug Martin <dougmartinrn@gmail.com>
2023-02-15 18:49:06 -0500

    Added memory and disk space checks

--- system-info.py
+++ system-info.py
@@ -1,22 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import requests
 import subprocess
 import paho.mqtt.client as mqtt
+from time import sleep
 
 # Get CPU temp
 t = str(subprocess.check_output(["vcgencmd", " measure_temp"]))
-cpuTemp = t[7:11] + "°C"
+cpuTemp = t[7:11] +  " °C"
+# print(cpuTemp)
 
-# Get uptime
-with open('/proc/uptime', 'r') as f:
-    u = float(f.readline().split()[0])
-    upTime = str(round(u/86400, 1)) + " days" # Converts seconds into days
+# Get free memory
+with open("/proc/meminfo", "r") as f:
+    m = float(f.readline().split()[1])
+    memFree = str(round(m/1000)) + " mb"
+    # print(memFree)
+
+# Get free disk space
+d = str(subprocess.check_output(["df", "-h", "/"])).split()[7]
+diskFree = str(d[:-1]) + " gb"
+# print(diskFree)
+
+
 
 # Send MQTT message for the Magic Mirror
-client = mqtt.Client("mirror") # Create a new instance
-client.connect("192.168.1.10") # Connect to the broker
+client = mqtt.Client("playground") # Create a new instance
+client.connect("playground.local") # Connect to the broker
 client.loop_start()
-client.publish("mirror/cpuTemp", cpuTemp) # Publish CPU temp
-client.publish("mirror/upTime", upTime) # Publish uptime
+client.publish("playground/cpuTemp", cpuTemp) # Publish CPU temp
+client.publish("playground/memFree", memFree) # Publish Free Memory
+client.publish("playground/diskFree", diskFree) # Publish Free Disk Space
 client.loop_stop()
