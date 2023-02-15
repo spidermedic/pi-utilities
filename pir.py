3d1da8aef932e286fbfb13e8d8fa2a5b9c63a7ac
Doug Martin <dougmartinrn@gmail.com>
2023-02-15 18:45:46 -0500

    Added descriptive variable names

--- pir.py
+++ pir.py
@@ -6,26 +6,26 @@ import subprocess
 import RPi.GPIO as GPIO
 
 # Initialize Variables
-PIR = 22          # Data pin that the PIR sensor is connected to
-timeout = 30      # How long to wait after no movement is detected to turn off the screen
+pir_gpio = 22     # Data pin that the pir_gpio sensor is connected to
+stay_awake = 30   # How long to wait after no movement is detected to turn off the screen
 display_on = True # Defaults the screen to "on"
 
 # Initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
-GPIO.setup(PIR, GPIO.IN)
+GPIO.setup(pir_gpio, GPIO.IN)
 
 # Main Function
 while True:
 
-    if display_on and not GPIO.input(PIR):
+    if display_on and not GPIO.input(pir_gpio):
         subprocess.run("vcgencmd display_power 0 > /dev/null 2>&1", shell=True)
         display_on = False
 
-    if not display_on and GPIO.input(PIR):
+    if not display_on and GPIO.input(pir_gpio):
         subprocess.run("vcgencmd display_power 1 > /dev/null 2>&1", shell=True)
         display_on = True
-        sleep(timeout)
+        sleep(stay_awake)
 
     sleep(0.5)
