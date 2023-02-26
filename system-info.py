391efa47135075d7a6b218c892bff1bbd3bff908
Doug Martin <dougmartinrn@gmail.com>
2023-02-25 22:37:06 -0500

    Divided into functions and added error handling.

--- system-info.py
+++ system-info.py
@@ -1,33 +1,47 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-import requests
 import subprocess
 import paho.mqtt.client as mqtt
-from time import sleep
-
-# Get CPU temp
-t = str(subprocess.check_output(["vcgencmd", " measure_temp"]))
-cpuTemp = t[7:11] +  " °C"
-# print(cpuTemp)
-
-# Get free memory
-with open("/proc/meminfo", "r") as f:
-    m = float(f.readline().split()[1])
-    memFree = str(round(m/1000)) + " mb"
-    # print(memFree)
-
-# Get free disk space
-d = str(subprocess.check_output(["df", "-h", "/"])).split()[7]
-diskFree = str(d[:-1]) + " gb"
-# print(diskFree)
 
 
+def cpu_temp():
+    try:
+        temp = str(subprocess.check_output(["vcgencmd", " measure_temp"]))
+        return f"{temp[7:11]} °C"
+    except:
+        return "-"
 
-# Send MQTT message for the Magic Mirror
-client = mqtt.Client("playground") # Create a new instance
-client.connect("playground.local") # Connect to the broker
-client.loop_start()
-client.publish("playground/cpuTemp", cpuTemp) # Publish CPU temp
-client.publish("playground/memFree", memFree) # Publish Free Memory
-client.publish("playground/diskFree", diskFree) # Publish Free Disk Space
-client.loop_stop()
+
+def free_memory():
+    try:
+        with open("/proc/meminfo", "r") as f:
+            memory = float(f.readline().split()[1])
+            return f"{str(round(memory/1000))} mb"
+    except:
+        return "-"
+    
+
+def free_disk_space():
+    try:
+        disk_free = str(subprocess.check_output(["df", "-h", "/"])).split()[7]
+        return f"{str(disk_free[:-1])} gb"
+    except:
+        return "-"
+
+def send_mqtt_data():
+    client = mqtt.Client("playground") # Create a new instance
+    client.connect("playground.local") # Connect to the broker
+    
+    client.loop_start()
+    client.publish("playground/cpuTemp", cpu_temp()) # Publish CPU temp
+    client.publish("playground/memFree", free_memory()) # Publish Free Memory
+    client.publish("playground/diskFree", free_disk_space()) # Publish Free Disk Space
+    client.loop_stop()
+
+
+def main():
+    send_mqtt_data()
+
+
+if __name__ == "__main__":
+    main()
