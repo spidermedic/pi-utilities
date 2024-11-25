89c3afd6cb73dccb30298f6b9706cf688e71e9f1
Doug Martin <33435021+spidermedic@users.noreply.github.com>
2024-11-24 21:54:41 -0500

    Updated pir.py

--- pir.py
+++ pir.py
@@ -1,4 +1,3 @@
-
 # Imported Modules
 from time import sleep
 import subprocess
@@ -17,14 +16,18 @@ display_on = True
 while True:
 
     if display_on and not GPIO.input(PIR_GPIO):
-        subprocess.run("xrandr --display :0 --output HDMI-1 --off > /dev/null 2>&1", shell=True)
-        # subprocess.run("vcgencmd display_power 0 > /dev/null 2>&1", shell=True)
+        # subprocess.run(["xset dpms force off"], shell=True)
+        # subprocess.run("xrandr --display :0 --output HDMI-1 --off > /dev/null 2>&1", shell=True)
+        subprocess.run("vcgencmd display_power 0 > /dev/null 2>&1", shell=True)
         display_on = False
+        print("Force Off Sent")
 
     if not display_on and GPIO.input(PIR_GPIO):
-        subprocess.run("xrandr --display :0 --output HDMI-1 --mode 1920x1080 --rate 60 > /dev/null 2>&1", shell=True)
-        # subprocess.run("vcgencmd display_power 0 > /dev/null 2>&1", shell=True)
+        # subprocess.run(["xset dpms force on"], shell=True)
+        # subprocess.run("xrandr --display :0 --output HDMI-1 --mode 1920x1080 --rate 60 > /dev/null 2>&1", shell=True)
+        subprocess.run("vcgencmd display_power 1 > /dev/null 2>&1", shell=True)
         display_on = True
+        print("Force On Sent")
         sleep(60)
 
     sleep(0.5)
